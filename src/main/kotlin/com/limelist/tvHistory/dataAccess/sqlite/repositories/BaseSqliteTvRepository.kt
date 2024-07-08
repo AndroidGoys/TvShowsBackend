7c54f41923ba6f545a840807d38af7bc1ce2ac85
WithsCat <da18.06.03@yandex.ru>
2024-07-08 10:01:23 +0300

    fixed db initialization

--- src/main/kotlin/com/limelist/tvHistory/dataAccess/sqlite/repositories/BaseSqliteTvRepository.kt
+++ src/main/kotlin/com/limelist/tvHistory/dataAccess/sqlite/repositories/BaseSqliteTvRepository.kt
@@ -16,6 +16,70 @@ abstract class BaseSqliteTvRepository(
         val releasesTableName = "releases"
     }
 
+    init {
+        val statement = connection.createStatement();
+
+        statement.execute("""
+            CREATE TABLE IF NOT EXISTS $channelsTabelName (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50) NOT NULL,
+                image_url TEXT NOT NULL,
+                description TEXT NOT NULL
+            );
+        """)
+
+        statement.execute("""
+            CREATE TABLE IF NOT EXISTS $showsTabelName (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50) NOT NULL,
+                age_limit INTEGER,
+                description TEXT NOT NULL,  
+                preview_url TEXT NOT NULL
+            );
+        """)
+
+        statement.execute("""
+            CREATE TABLE IF NOT EXISTS show_reviews (
+                id SERIAL PRIMARY KEY,
+                show_id INTEGER REFERENCES tv_shows,
+                date INTEGER,  
+                comment TEXT,
+                assessment INTEGER
+            );
+        """)
+
+        statement.execute("""
+            CREATE TABLE IF NOT EXISTS channel_reviews (
+                id SERIAL PRIMARY KEY,
+                channel_id INTEGER REFERENCES shows,
+                date INTEGER,  
+                comment TEXT,
+                assessment INTEGER
+            );
+        """)
+
+        statement.execute("""
+            CREATE TABLE IF NOT EXISTS $releasesTableName (
+                id SERIAL PRIMARY KEY,
+                show_id INTEGER REFERENCES shows,
+                channel_id INTEGER REFERENCES channels,
+                description TEXT NOT NULL,
+                time_start REAL,
+                time_stop REAL
+            );
+        """)
+
+
+        statement.execute("""
+            CREATE TABLE IF NOT EXISTS channel_view_urls (
+                id SERIAL PRIMARY KEY,
+                channel_id INTEGER REFERENCES channels,
+                url TEXT NOT NULL
+            );
+        """)
+        statement.close()
+    }
+
     val getCountStatement = connection.prepareStatement("""
            SELECT COUNT(*) as count FROM $tableName;
     """);
--- src/main/kotlin/com/limelist/tvHistory/dataAccess/sqlite/repositories/TvSqliteDbLifeCycle.kt
+++ src/main/kotlin/com/limelist/tvHistory/dataAccess/sqlite/repositories/TvSqliteDbLifeCycle.kt
@@ -13,68 +13,8 @@ class TvSqliteDbLifeCycle(
     val connection: Connection,
     val mutex: Mutex
 ) : DbLifeCycle {
-    override suspend fun start() = mutex.withLock {
-        val statement = connection.createStatement();
+    override suspend fun start() {
 
-        statement.execute("""
-            CREATE TABLE IF NOT EXISTS $channelsTabelName (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(50) NOT NULL,
-                image_url TEXT NOT NULL,
-                description TEXT NOT NULL
-            );
-        """)
-
-        statement.execute("""
-            CREATE TABLE IF NOT EXISTS $showsTabelName (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(50) NOT NULL,
-                age_limit INTEGER,
-                description TEXT NOT NULL,  
-                preview_url TEXT NOT NULL
-            );
-        """)
-
-        statement.execute("""
-            CREATE TABLE IF NOT EXISTS show_reviews (
-                id SERIAL PRIMARY KEY,
-                show_id INTEGER REFERENCES tv_shows,
-                date INTEGER,  
-                comment TEXT,
-                assessment INTEGER
-            );
-        """)
-
-        statement.execute("""
-            CREATE TABLE IF NOT EXISTS channel_reviews (
-                id SERIAL PRIMARY KEY,
-                channel_id INTEGER REFERENCES shows,
-                date INTEGER,  
-                comment TEXT,
-                assessment INTEGER
-            );
-        """)
-
-        statement.execute("""
-            CREATE TABLE IF NOT EXISTS $releasesTableName (
-                id SERIAL PRIMARY KEY,
-                show_id INTEGER REFERENCES shows,
-                channel_id INTEGER REFERENCES channels,
-                description TEXT NOT NULL,
-                time_start REAL,
-                time_stop REAL
-            );
-        """)
-
-
-        statement.execute("""
-            CREATE TABLE IF NOT EXISTS channel_view_urls (
-                id SERIAL PRIMARY KEY,
-                channel_id INTEGER REFERENCES channels,
-                url TEXT NOT NULL
-            );
-        """)
-        statement.close()
     }
 
     override suspend fun stop() {
